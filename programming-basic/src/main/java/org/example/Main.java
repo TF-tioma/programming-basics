0dafe22b9bea6d70e56bb0da7b663a89f1c5e682
TemurMalik <nomozovtemurmalik@gmail.com>
2023-01-20 00:01:14 +0500

    task 2,3,4 check

--- programming-basic/src/main/java/org/example/Main.java
+++ programming-basic/src/main/java/org/example/Main.java
@@ -11,11 +11,16 @@ public class Main {
          *
          *            линейный программъ -  chiziqli dastur
          */
-        task1();
-        task2();
+//        task1();
+//        task2();
+//        task3();
+//        task4();
+        task5();
+//        task6();
+//        task7();
     }
 
-    private static double scannerDouble(){
+    private static double scannerDouble() {
         Scanner in = new Scanner(System.in);
         double number;
         System.out.print("Insert the number: ");
@@ -28,10 +33,10 @@ public class Main {
         return number;
     }
 
-    public static int scannerInt(){
+    public static int scannerInt() {
         Scanner in = new Scanner(System.in);
         int number;
-        System.out.println("Insert the number: ");
+        System.out.print("Insert the number: ");
         while (!in.hasNextInt()) {
             in.next();
             System.out.println("Invalid input!");
@@ -41,17 +46,62 @@ public class Main {
         return number;
     }
 
-    //1-найдите значение функции z=((a-3)*b/2)+c.  -  funksiya qiymatini toping
-    public static void task1()  {
+    //1-найдите значение функции z=((a-3)*b/2)+c.
+    public static void task1() {
         double a = scannerDouble();
         double b = scannerDouble();
         double c = scannerDouble();
-        double z = ((a-3)*b/2)+c;
+        double z = ((a - 3) * b / 2) + c;
         System.out.println(z);
     }
 
-    public static void task2(){
+    //Вычислить значение выражения по формуле ( все переменные принимают действительные значения )
+    public static void task2() {
+        int a = scannerInt();
+        int b = scannerInt();
+        int c = scannerInt();
+        double z = (b + Math.sqrt(b * b + 4 * a * c)) / 2 * a - Math.pow(a, 3) * c + 1.0 / b * b;
+        System.out.println(z);
+    }
+
+    //Вычислить значение выражения по формуле ( все переменные принимают действительные значения )
+    public static void task3() {
+        double x = scannerDouble();
+        double y = scannerDouble();
+        double z = ((Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y))) * Math.tan(x * y);
+        System.out.println(z);
+    }
+
+    /**
+     *     4. Дано действительное число R вида nnn.ddd ( три цифровых разряда в дробной и целой частях ) .
+     *     Поменять местами дробную и целую части числа и вывести полученное значение числа .
+     */
+    public static void task4(){
+        double a = scannerDouble();
+        String s = String.valueOf(a);
+        System.out.println(s.substring(4) + s.charAt(3) + s.substring(0,3) );
+    }
+
+    /**
+     * 5. Дано натуральное число Т , которое представляет длительность прошедшего времени в секундах .
+     * Вывести Данное значение длительности в часах , минутах и секундах в следующей форме : Hнч Мммин SSC
+     */
+    public static void task5(){
 
     }
 
+    /**
+     *6. Для данной области составить линейную программу , которая печатает true ,
+     * если точка с координатами ( x , y ) принадлежит закрашенной области , и false - в противном случае :
+     */
+    public static void task6(){
+
+    }
+
+    /**
+     *
+     */
+    public static void task7(){
+
+    }
 }
